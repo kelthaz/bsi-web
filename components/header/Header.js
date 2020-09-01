a874dda123d2d4595687db8e5d4182a2f17979d5
Jesus David Gomez Osorno <jesus.gomez@ceiba.com.co>
2020-09-01 15:19:52 -0500

    GRAL: Se agregaron el estilo de los botones

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,5 +1,24 @@
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <button type="button" className="btn-big" disabled>
+        Solicita ya tu credito
+      </button>
+      <button type="button" className="btn-medium">
+        btn1
+      </button>
+      <button type="button" className="btn-small">
+        btn1
+      </button>
+      <button type="button" className="btn-mini">
+        btn1
+      </button>
+      <button type="button" className="btn-big-secondary">
+        btn1
+      </button>
+      Header
+    </div>
+  );
 };
 
 export default Header;
