97f9adcb717416b4f7f6c6a1cc3a2c630718e309
Mateo Leal <mateo.leal@ceiba.com.co>
2020-09-10 16:26:08 -0500

    DEVUS: PYMC-21: front|Crear banner (Implementación de dos componentes para los banners)

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,5 +1,16 @@
-export const Banner = () => {
-  return <div>Banner</div>;
+import styles from './banner.module.scss';
+
+export const Banner = (props) => {
+
+  let containerStyles = {
+    backgroundImage: `url(${props.backgroundImage})`
+  }
+
+  return <div style={containerStyles} className={styles.container}>
+    <div className={styles.content}>
+      {props.children}
+    </div>
+  </div>;
 };
 
 export default Banner;
