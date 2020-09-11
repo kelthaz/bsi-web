14b8bc17587bcfc2abec254457eb6b6715dae7ad
Mateo Leal <mateo.leal@ceiba.com.co>
2020-09-11 12:33:37 -0500

    DEVUS: PYMC-21: front|Crear banner (Eliminación de estilos inutilizados)

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -9,19 +9,15 @@ export const Banner = (props) => {
     backgroundSize: 'cover',
   }
 
-  // let isMobile = useMediaQuery('(max-width: 767px)');
-  let isMobile = false;
 
   return <div className={styles.container}>
     <div className={styles.content}>
-      {/* <div className={'row justify-content-center'}> */}
-        <div className={`${styles.text_block}`}>
-          {props.textBlock}
-        </div>
-        <div style={containerStyles} className={`${styles.image_block}`}>
-          {props.imageBlock}
-        </div>
-      {/* </div> */}
+      <div className={`${styles.text_block}`}>
+        {props.textBlock}
+      </div>
+      <div style={containerStyles} className={`${styles.image_block}`}>
+        {props.imageBlock}
+      </div>
     </div>
   </div>;
 };
