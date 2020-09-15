839f019dca76b15e46759b77d8ed35861e32e36e
Jesus David Gomez Osorno <jesus.gomez@ceiba.com.co>
2020-09-15 13:14:26 -0500

    DEVUS: PYMC-22 (Creación de la sección datos personales)

--- pages/credito-pyme/index.js
+++ pages/credito-pyme/index.js
@@ -1,9 +1,19 @@
 import Head from 'next/head';
 import { Footer } from '../../components/footer/Footer';
 import { Header } from '../../components/header/Header';
+import { TitleSection } from '../../components/title-section2/TitleSection';
+import styles from './credito-pyme.module.scss';
 
 const CreditoPyme = () => {
-  const a = 'dasd';
+  const data = {
+    steps: [
+      {
+        num: '01',
+        title: 'datos',
+      },
+      {},
+    ],
+  };
 
   return (
     <>
@@ -15,12 +25,22 @@ const CreditoPyme = () => {
       <article>
         <section className="section-blue-storm">
           <div className="container">
-            <div className="row">
-              <div className="col-8">
-                <button>dasd</button>
+            <div className="row p-lg-5 p-md-4 p-sm-2 p-xm-1">
+              <div className="col-lg-6 col-md-6 col-sm-12 col-xm-12">
+                <TitleSection className="pb-1" inverted="true" orden="02" linea1="Datos" linea2="personales" />
+                <div className="card-simple-white">
+                  <p>
+                    El primer bloque de Datos Personales es para conocerte, por lo que te solicitaremos información
+                    básica como tu nombre y tus datos de contacto.
+                  </p>
+                  <p>Para este bloque de la solicitud será indispensable contar con tu RFC con el que facturas.</p>
+                </div>
               </div>
-              <div className="col-4">
-                <button>dasd</button>
+
+              <div className="col-lg-6 col-md-6 col-sm-12 col-xm-12">
+                <div className={styles['container-svg']}>
+                  <img className="p-lg-5 p-md-4 p-sm-2 p-xm-1" src="/pantalla-circulo.svg" alt="" />
+                </div>
               </div>
             </div>
           </div>
@@ -29,7 +49,14 @@ const CreditoPyme = () => {
           <div className="container">
             <div className="row">
               <div className="col-8">
-                <button>dasd</button>
+                <div className="card-simple-white">
+                  <p>
+                    El primer bloque de Datos Personales es para conocerte, por lo que te solicitaremos información
+                    básica como tu nombre y tus datos de contacto.
+                    <br />
+                    Para este bloque de la solicitud será indispensable contar con tu RFC con el que facturas.
+                  </p>
+                </div>
               </div>
               <div className="col-4">
                 <button>dasd</button>
