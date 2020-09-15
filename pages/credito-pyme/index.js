ade8e47b495c4149db3515058be802b507245ffa
Jesus David Gomez Osorno <jesus.gomez@ceiba.com.co>
2020-09-14 19:47:04 -0500

    DEVUS: PYMC-20 (Creación de la sección)

---
+++ pages/credito-pyme/index.js
@@ -0,0 +1,95 @@
+import Head from 'next/head';
+import { Footer } from '../../components/footer/Footer';
+import { Header } from '../../components/header/Header';
+
+const CreditoPyme = () => {
+  const a = 'dasd';
+
+  return (
+    <>
+      <Head>
+        <title>Ayuda - BanCoppel | Pymes</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <article>
+        <section className="section-blue-storm">
+          <div className="container">
+            <div className="row">
+              <div className="col-8">
+                <button>dasd</button>
+              </div>
+              <div className="col-4">
+                <button>dasd</button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section-white">
+          <div className="container">
+            <div className="row">
+              <div className="col-8">
+                <button>dasd</button>
+              </div>
+              <div className="col-4">
+                <button>dasd</button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section-blue-night">
+          <div className="container">
+            <div className="row">
+              <div className="col-8">
+                <button>dasd</button>
+              </div>
+              <div className="col-4">
+                <button>dasd</button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section-white">
+          <div className="container">
+            <div className="row">
+              <div className="col-8">
+                <button>dasd</button>
+              </div>
+              <div className="col-4">
+                <button>dasd</button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section-blue-storm">
+          <div className="container">
+            <div className="row">
+              <div className="col-8">
+                <button>dasd</button>
+              </div>
+              <div className="col-4">
+                <button>dasd</button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section-blue-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-8">
+                <button>dasd</button>
+              </div>
+              <div className="col-4">
+                <button>dasd</button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </article>
+
+      <Footer />
+    </>
+  );
+};
+
+export default CreditoPyme;
