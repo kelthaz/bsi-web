ca8447078e9ae11808d737ec69f7875831c5ba06
Mateo Leal <mateo.leal@ceiba.com.co>
2020-09-15 14:48:25 -0500

    DEVUS: PYMC-2 (Implementación de landing de beneficios)

--- pages/index.js
+++ pages/index.js
@@ -6,24 +6,31 @@ import { TitleBanner } from '../components/title-banner/TitleBanner';
 import styles from './home.module.scss';
 
 const Home = () => {
-
-  let banner_text_block = (
+  const bannerTextBlock = (
     <>
-    <div className={`row justify-content-center ${styles.text_title}`}>
-      <TitleBanner linea1="Da el siguiente" linea2="gran salto"
-        description="Descubre un crédito diseñado para ti y las herramientas te ayudarán a elevar tu empresa."/>
-    </div>
-    <div className={'row justify-content-center mt-4'}>
-      <button type="button" className="btn-big">
-        Solicita ya tu crédito
-      </button>
-    </div>
-    </>);
+      <div className={`row justify-content-center ${styles.text_title}`}>
+        <TitleBanner
+          linea1="Da el siguiente"
+          linea2="gran salto"
+          description="Descubre un crédito diseñado para ti y las herramientas te ayudarán a elevar tu empresa."
+        />
+      </div>
+      <div className="row justify-content-center mt-4">
+        <button type="button" className="btn-big">
+          Solicita ya tu crédito
+        </button>
+      </div>
+    </>
+  );
 
-  let banner_image_block = (
+  const bannerImageBlock = (
     <div>
-      <div className={styles.banner_image_texture}><img src="/BC_PYMES_1 2.svg"/></div>
-      <div className={styles.banner_image_man}><img src="/BC_PYMES_1 1.png"/></div>
+      <div className={styles.banner_image_texture}>
+        <img src="/BC_PYMES_1 2.svg" alt="Textura del banner" />
+      </div>
+      <div className={styles.banner_image_man}>
+        <img src="/BC_PYMES_1 1.png" alt="Mujer" />
+      </div>
     </div>
   );
 
@@ -34,13 +41,12 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header/>
+      <Header />
 
-      <div className={`row justify-content-center ${styles.banner}`}>
-        <Banner backgroundImage={'/BC_PYMES_1.png'} textBlock={banner_text_block} imageBlock={banner_image_block}/>
-      </div>
+      <Banner backgroundImage="/BC_PYMES_1.png" textBlock={bannerTextBlock} imageBlock={bannerImageBlock} />
 
       <Footer />
-    </>);
-}
+    </>
+  );
+};
 export default Home;
