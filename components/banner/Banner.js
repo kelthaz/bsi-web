fbac75e1bae848fd87cec1ed41001e99d16ce9fb
Mateo Leal <mateo.leal@ceiba.com.co>
2020-09-17 12:34:06 -0500

    DEVD: PYMC-107 (Refactor banner)

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,27 +1,35 @@
+import PropTypes from 'prop-types';
 import styles from './banner.module.scss';
 
 export const Banner = (props) => {
-
-  let containerStyles = {
-    backgroundImage: `url(${props.backgroundImage})`,
+  const { backgroundImage, textBlock, imageBlock } = props;
+  const containerStyles = {
+    backgroundImage: `url(${backgroundImage})`,
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
     backgroundSize: 'cover',
-  }
+  };
 
-
-  return <div className={`row justify-content-center mx-0`}>
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <div className={`${styles.text_block}`}>
-          {props.textBlock}
-        </div>
-        <div style={containerStyles} className={`${styles.image_block}`}>
-          {props.imageBlock}
+  return (
+    <div className="row justify-content-center mx-0">
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles['text-block']}>
+            {textBlock}
+          </div>
+          <div style={containerStyles} className={styles['image-block']}>
+            {imageBlock}
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
+};
+
+Banner.propTypes = {
+  backgroundImage: PropTypes.string.isRequired,
+  textBlock: PropTypes.any.isRequired,
+  imageBlock: PropTypes.any.isRequired,
 };
 
 export default Banner;
