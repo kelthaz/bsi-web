fa54b240e5f2c4f4a36430c68e3ba7e1c82c7c35
David Prado <carlos.prado@the-cocktail.com>
2020-11-10 17:05:45 -0500

    DEVD: PYMC-386 prueba de push

--- helpers/uppercaseFormatter.js
+++ helpers/uppercaseFormatter.js
@@ -1,8 +1,9 @@
 const uppercaseFormatter = (value) =>
   value
     .normalize('NFD')
-    .replace(/[-$%&|<>#!@?^*()/'"{}|\s=+~_":;,.`]+$/, '')
-    .replace(/[\u0300-\u036f](?!\u0303)/g, '')
+    .replace(/[-$%&|<>#!@?^*()/'"{}|\s=+~_":;,.\\‘«{}❤❥💞💙💚💛💜😍😘😗😙😚😻💏😽💎´`[]+$/, '')
+    // .replace(/[\u0300-\u036f](?!\u0303)/g, '')
+    .replace(/[\u0300-\u0302\u036f]/g, '')
     .toUpperCase();
 
 export default uppercaseFormatter;
