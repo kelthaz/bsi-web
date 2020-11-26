7035a1e0ac64301774ef2503266d071ce16690e1
David Prado <carlos.prado@the-cocktail.com>
2020-11-26 15:16:19 -0500

    MERGE

--- components/features/solicitud/datos-empresa/step-eight/StepEight.js
+++ components/features/solicitud/datos-empresa/step-eight/StepEight.js
@@ -1,51 +1,157 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
 import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as Yup from 'yup';
+import Modal from '../../../../shared/modal/Modal';
+import TextField from '../../../../shared/text-field/TextField';
 import { nextStepDatosPersonales } from '../../../../../redux/actions/solicitud';
 import Link from 'next/link';
 
+import styles from './StepEight.module.scss';
+import SvgPrivacidad from '../../../../svgs/SvgPrivacidad';
+import CheckTextBox from '../../../../shared/check-text-box/CheckTextBox';
+
+import {
+  longitudMaxima,
+  campoRequerido,
+  longitudMinima,
+  aceptarTerminos,
+} from '../../../../../constants/errors';
+
 const StepEight = () => {
+  const [openWhyCiec, setOpenWhyCiec] = useState(false);
   const { datosEmpresa } = useSelector((state) => state.solicitud);
-  const dispatch = useDispatch();
   const router = useRouter();
 
+  const dispatch = useDispatch();
+
+  const { initialValues, validationSchema } = {
+    initialValues: {
+      ciec: datosEmpresa.ciec,
+      firmaElectronica: false
+    },
+    validationSchema: Yup.object({
+      ciec: Yup.string()
+        .max(20, longitudMaxima)
+        .min(7, longitudMinima)
+        .required(campoRequerido),
+        firmaElectronica: Yup.boolean()
+        .oneOf([true], aceptarTerminos)
+    }),
+  };
   const formulario = useFormik({
+    initialValues,
+    validationSchema,
     onSubmit: (values) => {
       dispatch(
         nextStepDatosPersonales({
-          currentStep: { tab: 'datos-empresa', step: '5' },
-          datosEmpresa: { ...datosEmpresa, ...values },
+          currentStep: { tab: 'datos-empresa', step: '8' },
+          datosEmpresa: {
+            ...datosEmpresa,
+            ...values
+          },
         })
       );
-      router.push('/solicitud/[tab]/[step]', '/solicitud/datos-empresa/7');
+      router.push('/solicitud/[tab]/[step]', '/solicitud/datos-empresa/9');
     },
-    validateOnMount: true,
   });
 
   return (
     <div className="contedor-fixed">
-      <div className="contedor-solicitud ">
+      <div className="contedor-solicitud">
         <div className="container p-0">
-          <div className="row mt-xs-5 mt-md-0">
-            <div className="col-md-12 col-xs-12 mt-xs-5 mt-md-0">
-              <h2 className="text-md-left color-blue-storm mt-5">¡Gracias por contarnos sobre ti!</h2>
-              <p className="body2 text-md-left color-dark-gray sub mt-4">
-                Ahora necesitamos tu autorización para la Consulta de Buró de Crédito, por lo que deberás tener a la
-                mano tu clave CIEC y tu e.firma.
+          <Modal openModal={openWhyCiec} setOpenModal={setOpenWhyCiec}>
+            <div className={styles['modal-container']}>
+              <h4 className="color-blue-storm">¿Qué es la CIEC y por qué solicitamos esto?</h4>
+              <p className="dark-gray body2">
+                Tu historial crediticio nos ayuda a diseñar tu oferta en segundos, por lo que requerimos
+                tus credenciales del SAT para que firmes la autorización y poder consultarlo.
               </p>
+              <p className="sub color-gray">
+                <SvgPrivacidad /> Tus datos estarán protegidos.
+              </p>
+              <iframe
+                className={styles['modal-video']}
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/r7HHOYZQb4M"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="¿Qué es la CIEC y por qué solicitamos esto?"
+               />
             </div>
+<<<<<<< HEAD
             <div className="text-center offset-md-4 offset-xs-3 my-3 ">
               <Link href="/simulador">
                 <button className="btn-medium" type="submit" aria-label="Avanzar">
                   <span>De acuerdo</span>
                 </button>
               </Link>
+=======
+          </Modal>
+          <form onSubmit={formulario.handleSubmit} noValidate>
+            <p className="color-dark-gray sub">
+              Primero necesitamos que nos autorices acceso con tu clave CIEC.<br />
+              <a className="sub link" onClick={() => setOpenWhyCiec(true)} role="button" tabIndex="0">¿Por qué te pedimos esto?</a>
+            </p>
+
+            <div className="row no-gutters">
+              <div className="col-lg-5 col-md-6 col-sm-12 col-xs-12 ">
+                <p className="input color-gray">Mi CIEC es</p>
+              </div>
+              <div className="col-lg-5 col-md-5 col-xs-12 pb-sm-3 pb-xs-3">
+                <TextField
+                  name="ciec"
+                  format="passwordspace"
+                  maxlength={20}
+                  formulario={formulario}
+                  type="password"
+                  size="big"
+                  label="**********"
+                />
+              </div>
+
+              <div className="card-simple">
+                <div className="row">
+                  <SvgPrivacidad />
+                  <p className="col-11">
+                    Tus datos estarán protegidos.<br />
+                    Cualquier duda te invitamos a conocer más sobre tu CIEC en la página oficial del SAT haciendo{' '}
+                    <a className="btn-link-blue" target="_blank" rel="noreferrer">
+                      clic aquí
+                    </a>.
+                  </p>
+                </div>
+              </div>
+
+              <div className="card-simple-gray">
+                <div className="row">
+                  <CheckTextBox name="firmaElectronica" formulario={formulario}>
+                    <p className="body3 color-gray mb-0">
+                      Acepto <a className="btn-link-blue" target="_blank" rel="noreferrer">
+                        términos y condiciones
+                      </a> de BanCoppel, en específico el uso de mi CIEC para manifestar mi voluntad por medios electrónicos.
+                    </p>
+                  </CheckTextBox>
+                </div>
+              </div>
             </div>
-          </div>
+            <div className="flex-column-center-config pt-sm-5 pt-xs-5 pt-md-0 pt-lg-0">
+              <button
+                type="submit"
+                className="cicle-button-blue my-3"
+                aria-label="Avanzar"
+                disabled={!(formulario.isValid && formulario.dirty)}
+              />
+>>>>>>> staging
+            </div>
+          </form>
         </div>
       </div>
     </div>
   );
 };
+
 export default StepEight;
---
+++ components/svgs/icons-cards/SvgNoGuardamosNada.js
@@ -0,0 +1,13 @@
+const SvgNoGuardamosNada = () => (
+  <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M38.8589 4.96582C30.0441 4.96582 22.8994 12.6432 22.8994 22.1153V26.0995H54.8183V22.1153C54.8183 12.6432 47.6736 4.96582 38.8589 4.96582Z" stroke="#81C1EA" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M58.7159 26.0996C60.9276 26.0996 62.717 28.0259 62.717 30.3991V53.816C62.717 59.7923 59.6702 65.2836 54.7792 68.1245L46.0579 73.1897C41.5667 75.7985 36.1534 75.7985 31.6622 73.1897L22.9409 68.1245" stroke="#225AA7" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M22.9378 68.1279C18.0468 65.287 15 59.7922 15 53.8194V30.3991C15 28.0259 16.7926 26.0996 19.0011 26.0996H58.7128" stroke="#FFBE12" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M42.9998 44.9997C42.9998 47.577 40.9105 49.6663 38.3332 49.6663C35.7558 49.6663 33.6665 47.577 33.6665 44.9997C33.6665 42.4223 35.7558 40.333 38.3332 40.333C40.9105 40.333 42.9998 42.4223 42.9998 44.9997Z" fill="#225AA7" stroke="#225AA7" stroke-width="4"/>
+    <path d="M38.3333 56.3337C37.4837 56.3337 37 55.7121 37 55.1928V49.8079C37 49.2886 37.4837 48.667 38.3333 48.667C39.183 48.667 39.6667 49.2886 39.6667 49.8079V55.1928C39.6667 55.7121 39.183 56.3337 38.3333 56.3337Z" fill="#225AA7" stroke="#225AA7" stroke-width="4"/>
+  </svg>
+
+
+);
+
+export default SvgNoGuardamosNada;
