5eec777202fe36b0bc14c46081b41136c1ca7c02
Jesus David Gomez Osorno <jesus.gomez@the-cocktail.com>
2020-11-26 15:18:20 -0500

    DEVD:  PYMC-30 ( radio button)

--- components/features/solicitud/datos-empresa/step-two/StepTwo.js
+++ components/features/solicitud/datos-empresa/step-two/StepTwo.js
@@ -1,12 +1,12 @@
 import { useFormik } from 'formik';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as Yup from 'yup';
 import { campoRequerido, longitudMaxima, seleccionOpcion } from '../../../../../constants/errors';
 import { nextStepDatosPersonales } from '../../../../../redux/actions/solicitud';
+import RadioButton from '../../../../shared/radio-button/RadioButton';
 import Select from '../../../../shared/select/Select';
-import TextArea from '../../../../shared/text-area/TextArea';
 import TextField from '../../../../shared/text-field/TextField';
 import Tooltip from '../../../../shared/tooltip/Tooltip';
 
@@ -14,21 +14,36 @@ const StepTwo = () => {
   const { datosEmpresa } = useSelector((state) => state.solicitud);
   const dispatch = useDispatch();
   const router = useRouter();
+  const [itemsGiro, setItemsGiro] = useState([]);
 
   const formulario = useFormik({
     initialValues: {
-      usoCredito: datosEmpresa.usoCredito,
-      descripcionCredito: datosEmpresa.descripcionCredito,
+      calle: datosEmpresa.direccionFiscal.calle,
+      numExterior: datosEmpresa.direccionFiscal.numExterior,
+      numInterior: datosEmpresa.direccionFiscal.numInterior,
+      codigoPostal: datosEmpresa.direccionFiscal.codigoPostal,
+      colonia: datosEmpresa.direccionFiscal.colonia,
+      municipioAlcaldia: datosEmpresa.direccionFiscal.municipioAlcaldia,
+      ciudad: datosEmpresa.direccionFiscal.ciudad,
+      estado: datosEmpresa.direccionFiscal.estado,
+      esDomilicioComercial: datosEmpresa.esDomilicioComercial,
     },
     validationSchema: Yup.object({
-      usoCredito: Yup.object()
+      calle: Yup.string(),
+      numExterior: Yup.string(),
+      numInterior: Yup.string(),
+      codigoPostal: Yup.string(),
+      colonia: Yup.object()
         .shape({
           value: Yup.string(),
           label: Yup.string(),
         })
         .nullable()
         .required(seleccionOpcion),
-      descripcionCredito: Yup.string().trim().max(180, longitudMaxima).required(campoRequerido),
+      municipioAlcaldia: Yup.string(),
+      ciudad: Yup.string(),
+      estado: Yup.string(),
+      esDomilicioComercial: Yup.string(),
     }),
     onSubmit: (values) => {
       dispatch(
@@ -44,14 +59,36 @@ const StepTwo = () => {
     },
   });
 
-  const { values, setFieldTouched, setFieldValue, touched } = formulario;
-
-  const handleUsoCredito = (usoCredito) => {
-    if (!touched.usoCredito) {
-      setFieldTouched('usoCredito', true);
-    }
-    setFieldValue('usoCredito', usoCredito);
-  };
+  const formularioAuxiliar = useFormik({
+    initialValues: {
+      calle: datosEmpresa.domiciolioComercial.calle,
+      numExterior: datosEmpresa.domiciolioComercial.numExterior,
+      numInterior: datosEmpresa.domiciolioComercial.numInterior,
+      codigoPostal: datosEmpresa.domiciolioComercial.codigoPostal,
+      colonia: datosEmpresa.domiciolioComercial.colonia,
+      municipioAlcaldia: datosEmpresa.domiciolioComercial.municipioAlcaldia,
+      ciudad: datosEmpresa.domiciolioComercial.ciudad,
+      estado: datosEmpresa.domiciolioComercial.estado,
+      domicilioEntrega: datosEmpresa.domicilioEntrega,
+    },
+    validationSchema: Yup.object({
+      calle: Yup.string(),
+      numExterior: Yup.string(),
+      numInterior: Yup.string(),
+      codigoPostal: Yup.string(),
+      colonia: Yup.object()
+        .shape({
+          value: Yup.string(),
+          label: Yup.string(),
+        })
+        .nullable()
+        .required(seleccionOpcion),
+      municipioAlcaldia: Yup.string(),
+      ciudad: Yup.string(),
+      estado: Yup.string(),
+      domicilioEntrega: Yup.string(),
+    }),
+  });
 
   return (
     <div className="contedor-fixed-tab">
@@ -64,101 +101,80 @@ const StepTwo = () => {
             </p>
 
             <div className="row no-gutters">
-              <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-                <TextField
-                  name="razonSocial"
-                  maxlength={120}
-                  formulario={formulario}
-                  type="text"
-                  size="big"
-                  label="Razón social"
-                />
+              <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6 pr-lg-2 pr-md-2">
+                <TextField name="calle" maxlength={60} formulario={formulario} type="text" size="big" label="Calle" />
+              </div>
+              <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6 pr-lg-2 pr-md-2">
+                <TextField name="numExterior" maxlength={6} formulario={formulario} type="text" size="big" label="#" />
               </div>
               <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6">
                 <TextField
-                  name="razonSocial"
-                  maxlength={120}
+                  name="numInterior"
+                  maxlength={6}
                   formulario={formulario}
                   type="text"
                   size="big"
-                  label="Razón social"
-                />
-              </div>
-              <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-                <TextField
-                  name="razonSocial"
-                  maxlength={120}
-                  formulario={formulario}
-                  type="text"
-                  size="big"
-                  label="Razón social"
+                  label="Int."
                 />
               </div>
 
-              <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+              <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6 pr-lg-2 pr-md-2">
                 <TextField
-                  name="razonSocial"
-                  maxlength={120}
+                  name="codigoPostal"
+                  maxlength={5}
                   formulario={formulario}
                   type="text"
                   size="big"
-                  label="Razón social"
+                  label="C.P"
                 />
               </div>
               <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
                 <Select
-                  name="razonSocial"
+                  name="colonia"
                   maxlength={120}
                   formulario={formulario}
                   type="text"
                   size="big"
-                  label="Razón social"
+                  items={itemsGiro}
+                  label="Colonia"
+                  disabled={itemsGiro.length === 0}
                 />
               </div>
 
-              <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+              <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 pr-lg-2 pr-md-2">
                 <TextField
-                  name="razonSocial"
-                  maxlength={120}
+                  name="municipioAlcaldia"
+                  maxlength={50}
                   formulario={formulario}
                   type="text"
                   size="big"
-                  label="Razón social"
+                  label="Municipio/Alcaldía"
                 />
               </div>
               <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <Select
-                  name="razonSocial"
-                  maxlength={120}
-                  formulario={formulario}
-                  type="text"
-                  size="big"
-                  label="Razón social"
-                />
+                <TextField name="ciudad" maxlength={50} formulario={formulario} type="text" size="big" label="Ciudad" />
               </div>
 
               <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <TextField
-                  name="razonSocial"
-                  maxlength={120}
-                  formulario={formulario}
-                  type="text"
-                  size="big"
-                  label="Razón social"
-                />
+                <TextField name="estado" maxlength={50} formulario={formulario} type="text" size="big" label="Estado" />
               </div>
             </div>
 
             <p className="color-gray-dark body2">¿Éste es también tu domicilio comercial?</p>
 
-            <div className="row">
-              <TextArea
-                name="descripcionCredito"
-                maxlength={180}
-                formulario={formulario}
-                label="Platícanos un poco más sobre qué harás con este crédito..."
-              />
+            <div className="row no-gutters">
+              <div className="col-lg-2 col-md-2 col-sm-6 col-xs-6 pr-lg-2 pr-md-2">
+                <RadioButton name="esDomilicioComercial" formulario={formulario} value="si">
+                  <p className="input color-gray m-0">Sí</p>
+                </RadioButton>
+              </div>
+              <div className="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+                <RadioButton name="esDomilicioComercial" formulario={formulario} value="no">
+                  <p className="input color-gray m-0">No</p>
+                </RadioButton>
+              </div>
             </div>
+
             <div className="flex-column-center-config pt-sm-5 pt-xs-5 pt-md-0 pt-lg-0">
               <button
                 type="submit"
--- components/shared/radio-button/RadioButton.js
+++ components/shared/radio-button/RadioButton.js
@@ -2,31 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './radio-button.module.scss';
 
-const RadioButton = ({ name, formulario, children }) => {
-  const { values, setFieldTouched, handleChange, errors, touched } = formulario;
+const RadioButton = ({ name, formulario, children, value }) => {
+  const { values, setFieldTouched, handleChange, errors, touched, setFieldValue } = formulario;
 
   const handleOnChange = (event) => {
     if (!touched[name]) {
       setFieldTouched(name, true);
     }
-    handleChange(event);
-  };
 
+    console.log(event.target.value);
+    setFieldValue(name, event.target.value);
+  };
+  console.log(values[name]);
   return (
     <>
-      <div className={`card-simple-blue-light ${styles['container-check-text']}`}>
+      <div className={`${styles['container-check-text']}`}>
         <div className={`${styles['container-check']}`}>
           <input
             id={name}
             name={name}
             className={` ${styles['my-check']}`}
             type="radio"
-            onChange={handleOnChange}
-            value={values[name]}
+            value={value}
+            onClick={handleOnChange}
           />
-          <label htmlFor={name} className={`${styles.label}`}>
-            {' '}
-          </label>
         </div>
         <div>{children}</div>
       </div>
