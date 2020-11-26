36bcc45389699b4893346442185ed5550f4bbe81
Mateo Leal <mateo.leal@the-cocktail.com>
2020-11-26 13:19:19 -0500

    DEVUS: PYMC-575: Front|integracion|Empresas pantalla 10,11 y 12 Contrato buro de credito (Implementación de pantallas de empresas para el onboarding)

---
+++ components/svgs/SvgBuroRevision.js
@@ -0,0 +1,24 @@
+const SvgBuroRevision = () => (
+  <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <circle opacity="0.5" cx="50" cy="50" r="50" fill="url(#paint0_linear)"/>
+  <path d="M37.8633 39.0322H61.6289" stroke="#225AA7" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round"/>
+  <path d="M37.9521 50.458H54.2021" stroke="#225AA7" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round"/>
+  <path d="M37.9521 61.3955H49.3271" stroke="#225AA7" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round"/>
+  <path d="M49.7461 77.1182H28.904C26.1945 77.1182 24 75.0875 24 72.5803V28.3358C24 25.8286 26.1945 23.7979 28.904 23.7979H49.7461" stroke="#81C1EA" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round"/>
+  <path d="M35.8828 23.7979H70.6172C73.3106 23.7979 75.4922 25.8286 75.4922 28.3358V72.5803C75.4922 75.0875 73.3106 77.1182 70.6172 77.1182H49.8984" stroke="#225AA7" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round"/>
+  <path d="M58.125 59.6377L67.3672 67.8408L85.8516 48.7979" stroke="#FFBE12" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+  <path d="M17.1523 7.5V20.9189" stroke="#FFBE12" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+  <path d="M13.1582 14.209H21.1486" stroke="#FFBE12" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+  <path d="M13.1582 14.209H21.1486" stroke="#FFBE12" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+  <path d="M85.1924 77.6445V91.0635" stroke="#FFBE12" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+  <path d="M81.1982 84.3545H89.1886" stroke="#FFBE12" strokeWidth="4" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+  <defs>
+  <linearGradient id="paint0_linear" x1="97.8916" y1="-36.2069" x2="64.9235" y2="103.715" gradientUnits="userSpaceOnUse">
+  <stop stopColor="white"/>
+  <stop offset="1" stopColor="#B5C8E9"/>
+  </linearGradient>
+  </defs>
+  </svg>
+);
+
+export default SvgBuroRevision;
---
+++ components/svgs/icons-cards/SvgFirmaElectronica.js
@@ -0,0 +1,20 @@
+const SvgFirmaElectronica = (props) => (
+  <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M63.3164 6C67.2073 6 70.3878 9.18047 70.3878 13.0714V50.0943" stroke="#225AA7" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M6.89941 46.1035V13.0714C6.89941 9.18047 10.0799 6 13.9708 6H63.3195" stroke="#81C1EA" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M47.3174 71.0002H13.9708C10.0799 71.0002 6.89941 67.8197 6.89941 63.9288V46.1006" stroke="#225AA7" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M43.0787 22.9756C43.7015 24.0035 44.0643 25.243 44.0643 26.5793C44.0643 30.1377 41.4794 33.0219 38.2869 33.0219C36.0255 33.0219 34.0694 31.5737 33.1201 29.4635" stroke="#81C1EA" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M33.1234 29.4665C32.7334 28.5988 32.5127 27.6193 32.5127 26.5793C32.5127 23.0209 35.0976 20.1367 38.2901 20.1367C40.2855 20.1367 42.042 21.2644 43.079 22.9786" stroke="#81C1EA" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M49.0924 40.7671C48.137 39.8087 46.8642 39.074 45.54 38.5117" stroke="#338CBF" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M45.5398 38.5085C41.2015 36.6703 36.2675 36.6703 31.9291 38.5085C30.747 39.0104 29.6073 39.6483 28.6973 40.4585" stroke="#81C1EA" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M18.8266 59.5119C17.4208 59.5119 16.272 58.363 16.272 56.9572V56.3193C16.272 54.9135 17.4208 53.7646 18.8266 53.7646H36.7364" stroke="#81C1EA" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M36.7334 53.7676H57.5546C58.9604 53.7676 60.1093 54.9164 60.1093 56.3222V56.9601C60.1093 58.366 58.9604 59.5148 57.5546 59.5148H18.8267" stroke="#81C1EA" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M50.7366 18.9336C53.001 21.6576 54.3645 25.1585 54.3645 28.9769C54.3645 33.6599 52.3178 37.8622 49.0708 40.7434" stroke="#225AA7" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M24.3982 35.6371C23.4519 33.6145 22.9229 31.3561 22.9229 28.9768C22.9229 20.294 29.961 13.2559 38.6438 13.2559C43.5082 13.2559 47.8527 15.4628 50.7368 18.9335" stroke="#81C1EA" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M49.0713 40.743C46.2959 43.2039 42.6438 44.6974 38.644 44.6974C32.3436 44.6974 26.9077 40.9909 24.3984 35.6367" stroke="#225AA7" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M50.7368 63.203L58.5338 71L75.1617 54.3721" stroke="#FFBE12" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+
+);
+
+export default SvgFirmaElectronica;
