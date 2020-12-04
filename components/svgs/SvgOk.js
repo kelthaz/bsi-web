9c075c3c40e7c64899c22ec61ffa010ff4a6aacf
David Prado <carlos.prado@the-cocktail.com>
2020-12-04 11:11:31 -0500

    SVG en agradecimiento

---
+++ components/svgs/SvgOk.js
@@ -0,0 +1,73 @@
+import * as React from 'react';
+
+function SvgOk(props) {
+  return (
+    <svg width={137} height={137} viewBox="0 0 137 137" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <ellipse cx={59.553} cy={59.371} rx={59.553} ry={59.371} fill="#F4F6F9" />
+      <circle opacity={0.5} cx={79.742} cy={79.865} fill="url(#prefix__paint0_linear)" r={56.313} />
+      <path
+        d="M92.326 89.564c1.025-3.573 1.2-7.326.668-10.993l-5.333-36.57c-.683-4.682-5.298-6.36-8.994-4.177M86.53 116.131l.064-14.205 3.981-8.166a26.232 26.232 0 001.63-4.238"
+        stroke="#225AA7"
+        strokeWidth={4}
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M83.389 58.319l-7.452-27.38c-2.131-6.415-8.312-8.528-12.786-4.299"
+        stroke="#81C1EA"
+        strokeWidth={4}
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M73.623 59.054L62.06 22.983c-1.376-4.298-6.764-5.65-10.04-2.518a6.185 6.185 0 00-1.664 6.203l7.868 26.796"
+        stroke="#81C1EA"
+        strokeWidth={4}
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M41.23 90.41a56.56 56.56 0 007.636 8.666l3.32 3.066-.074 13.915 34.438.022"
+        stroke="#225AA7"
+        strokeWidth={4}
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M65.68 56.2l-9.114-3.598a12.064 12.064 0 00-11.102 1.186l-9.765 6.483c-3.183 2.113-2.598 7.04 1.028 8.175 2.21.69 4.996.694 8.322-.957 1.004-.498 2.05-.898 3.145-1.135 2.887-.627 7.86-.954 10.41 3.26 1.738 2.869 1.038 6.6-1.51 8.803-1.431 1.236-3.339 2.2-5.665 1.825-1.733-.28-3.249-1.309-4.285-2.729-1.341-1.843-4.103-5.15-7.49-6.5-3.84-1.531-7.538 2.626-5.71 6.35l2.554 5.202a56.461 56.461 0 004.788 7.995"
+        stroke="#81C1EA"
+        strokeWidth={4}
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.438 45.47l.04 15.097M6.586 53.037l9.748-.04M100.895 9.984l-.016 23.596M93.266 21.79l15.235-.012"
+        stroke="#FFBE12"
+        strokeWidth={4}
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <defs>
+        <linearGradient
+          id="prefix__paint0_linear"
+          x1={133.68}
+          y1={-17.225}
+          x2={96.55}
+          y2={140.362}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#fff" />
+          <stop offset={1} stopColor="#B5C8E9" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default SvgOk;
