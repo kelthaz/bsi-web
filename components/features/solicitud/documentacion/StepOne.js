5e190ce81bf561c1d9380cd2fe70228d86f783c3
David Prado <carlos.prado@the-cocktail.com>
2020-12-14 09:45:20 -0500

    DEVD: PYMC-637, 658 | se corrigen las incidencias reportadas

--- components/features/solicitud/documentacion/StepOne.js
+++ components/features/solicitud/documentacion/StepOne.js
@@ -53,81 +53,83 @@ const StepOne = () => {
   };
 
   return (
-    <div className="contedor-fixed-tab">
-      <div className="contedor-solicitud">
-        <div className="container p-0">
-          <form onSubmit={formulario.handleSubmit} noValidate>
-            <Modal openModal={openConfirmation} setOpenModal={setOpenConfirmation}>
-              <div className={`px-0 ${styles['modal-container-video']}`}>
-                <div>
-                  <h4 className="color-blue-storm">¿Quién es un Obligado Solidario y por qué solicitamos uno?</h4>
-                  <p className="dark-gray body2">
-                    Un obligado solidario es una persona que se compromete a liquidar el crédito que estás solicitando
-                    si por algún motivo tú no pudieras.
-                  </p>
-                </div>
-                <iframe
-                  className={styles['modal-video']}
-                  width="560"
-                  height="315"
-                  src="https://www.youtube.com/embed/r7HHOYZQb4M"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                  title="¿Qué es la CIEC y por qué solicitamos esto?"
-                />
-              </div>
-            </Modal>
-            <p className="color-dark-gray sub position-relative">
-              Vamos a platicar un poco sobre la persona que te gustaría designar como tu Obligado Solidario.{' '}
-              <a onClick={() => setOpenConfirmation(true)} className="link">
-                ¿Porqué te pedimos esto?
-              </a>
+    <>
+      <Modal openModal={openConfirmation} setOpenModal={setOpenConfirmation}>
+        <div className={`px-0 ${styles['modal-container-video']}`}>
+          <div>
+            <h4 className="color-blue-storm">¿Quién es un Obligado Solidario y por qué solicitamos uno?</h4>
+            <p className="dark-gray body2">
+              Un obligado solidario es una persona que se compromete a liquidar el crédito que estás solicitando si por
+              algún motivo tú no pudieras.
             </p>
-            <p className="color-dark-gray sub position-relative">Antes que nada, ¿Qué tipo de persona es?</p>
+          </div>
+          <iframe
+            className={styles['modal-video']}
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/r7HHOYZQb4M"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="¿Qué es la CIEC y por qué solicitamos esto?"
+          />
+        </div>
+      </Modal>
+      <div className="contedor-fixed-tab">
+        <div className="contedor-solicitud">
+          <div className="container p-0">
+            <form onSubmit={formulario.handleSubmit} noValidate>
+              <p className="color-dark-gray sub position-relative">
+                Vamos a platicar un poco sobre la persona que te gustaría designar como tu Obligado Solidario.{' '}
+                <a onClick={() => setOpenConfirmation(true)} className="link">
+                  ¿Porqué te pedimos esto?
+                </a>
+              </p>
+              <p className="color-dark-gray sub position-relative">Antes que nada, ¿Qué tipo de persona es?</p>
 
-            <div className="row my-3">
-              <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 pl-lg-5 pl-md-5  mb-sm-2 mb-xs-2">
-                <button
-                  type="button"
-                  className={`card-simple-white-svg card-button ${
-                    values.usoCredito && values.usoCredito.value === capitalTrabajo.value && 'card-selected-blue-sky'
-                  }`}
-                  onClick={() => handleUsoCredito(capitalTrabajo)}
-                >
-                  <div>
-                    <SvgPersonaFisicaActividadFisica />
-                  </div>
+              <div className="row my-3">
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 pl-lg-5 pl-md-5  mb-sm-2 mb-xs-2">
+                  <button
+                    type="button"
+                    className={`card-simple-white-svg card-button ${
+                      values.usoCredito && values.usoCredito.value === capitalTrabajo.value && 'card-selected-blue-sky'
+                    }`}
+                    onClick={() => handleUsoCredito(capitalTrabajo)}
+                  >
+                    <div>
+                      <SvgPersonaFisicaActividadFisica />
+                    </div>
 
-                  <p className="px-md-5 px-lg-5">{capitalTrabajo.label}</p>
-                  <span className="px-md-5 px-lg-5 color-gray">{capitalTrabajo.subLabel}</span>
-                </button>
+                    <p className="px-md-5 px-lg-5">{capitalTrabajo.label}</p>
+                    <span className="px-md-5 px-lg-5 color-gray">{capitalTrabajo.subLabel}</span>
+                  </button>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 pr-lg-5 pr-md-5 mb-sm-2 mb-xs-2">
+                  <button
+                    type="button"
+                    className={`card-simple-white-svg card-button ${
+                      values.usoCredito &&
+                      values.usoCredito.value === adquisicionActivacion.value &&
+                      'card-selected-blue-sky'
+                    }`}
+                    onClick={() => handleUsoCredito(adquisicionActivacion)}
+                  >
+                    <div>
+                      <SvgPersonaMoralBlue />
+                    </div>
+                    <p className="px-md-5 px-lg-5">{adquisicionActivacion.label}</p>
+                    <span className="px-md-5 px-lg-5 color-gray">{adquisicionActivacion.subLabel}</span>
+                  </button>
+                </div>
               </div>
-              <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 pr-lg-5 pr-md-5 mb-sm-2 mb-xs-2">
-                <button
-                  type="button"
-                  className={`card-simple-white-svg card-button ${
-                    values.usoCredito &&
-                    values.usoCredito.value === adquisicionActivacion.value &&
-                    'card-selected-blue-sky'
-                  }`}
-                  onClick={() => handleUsoCredito(adquisicionActivacion)}
-                >
-                  <div>
-                    <SvgPersonaMoralBlue />
-                  </div>
-                  <p className="px-md-5 px-lg-5">{adquisicionActivacion.label}</p>
-                  <span className="px-md-5 px-lg-5 color-gray">{adquisicionActivacion.subLabel}</span>
-                </button>
+              <div className="flex-column-center-config pt-sm-5 pt-xs-5 pt-md-0 pt-lg-0">
+                <button type="submit" className="cicle-button-blue my-3" aria-label="Avanzar" />
               </div>
-            </div>
-            <div className="flex-column-center-config pt-sm-5 pt-xs-5 pt-md-0 pt-lg-0">
-              <button type="submit" className="cicle-button-blue my-3" aria-label="Avanzar" />
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- components/features/solicitud/documentacion/StepTwo.js
+++ components/features/solicitud/documentacion/StepTwo.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
+import Link from 'next/link';
 import * as Yup from 'yup';
 import { useRouter } from 'next/router';
 import Modal from '../../../shared/modal/Modal';
@@ -52,127 +53,131 @@ const StepTwo = () => {
   });
 
   return (
-    <div className="contedor-fixed-tab">
-      <div className="contedor-solicitud ">
-        <div className="container p-0 mt-5">
-          <form className="mt-xs-5 mt-md-0 mt-lg-0" onSubmit={formulario.handleSubmit} noValidate>
-            <Modal openModal={openConfirmation} setOpenModal={setOpenConfirmation}>
-              <div className={styles['modal-container']}>
-                <div className="d-flex justify-content-center pb-4">
-                  <SvgEnviado />
-                </div>
-                <div>
-                  <h4 className="color-blue-storm">¡Invitación enviada!</h4>
-                  <p className="dark-gray body2">
-                    Hemos enviado un correo a tu obligado solidario
-                    <br />
-                    José Álvarez Ruiz.
-                  </p>
-                </div>
-                <div className="d-flex justify-content-center">
-                  <button className="btn-medium" type="submit" aria-label="Avanzar">
-                    <span>Continuemos</span>
-                  </button>
-                </div>
-              </div>
-            </Modal>
-            <h2 className="color-blue-storm">¡Anotado!</h2>
-            <p className="color-dark-gray sub">
-              También requerimos que tu Obligado Solidario realice un proceso similar al tuyo, por lo que te pedimos nos
-              compartas sus datos para invitarle.
+    <>
+      <Modal openModal={openConfirmation} setOpenModal={setOpenConfirmation}>
+        <div className={styles['modal-container']}>
+          <div className="d-flex justify-content-center pb-4">
+            <SvgEnviado />
+          </div>
+          <div>
+            <h4 className="color-blue-storm">¡Invitación enviada!</h4>
+            <p className="dark-gray body2">
+              Hemos enviado un correo a tu obligado solidario
+              <br />
+              {datosEmpresa.primerNombreRecibe}.
             </p>
-
-            <div className="row no-gutters">
-              <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 ">
-                <p className="input color-gray">Mi nombre es</p>
-              </div>
-              <div className="col-lg-4 col-md-4  col-xs-12 pr-lg-2 pr-md-2 pb-sm-3 pb-xs-3">
-                <TextField
-                  name="primerNombreDoc"
-                  format="uppercase"
-                  maxlength={12}
-                  formulario={formulario}
-                  type="text"
-                  size="big"
-                  label="Nombre"
-                />
-              </div>
-              <div className="col-lg-4 col-md-4  col-xs-12 pr-lg-2 pr-md-2 pb-sm-3 pb-xs-3">
-                <TextField
-                  name="segundoNombreDoc"
-                  format="uppercase"
-                  maxlength={60}
-                  formulario={formulario}
-                  type="text"
-                  size="big"
-                  label="Nombre"
-                  optional
-                />
-              </div>
-              <div className="col-lg-6 col-md-6  col-xs-12 pr-lg-2 pr-md-2 pb-sm-3 pb-xs-3">
-                <TextField
-                  name="primerApellidoDoc"
-                  format="uppercase"
-                  maxlength={20}
-                  formulario={formulario}
-                  type="text"
-                  size="big"
-                  label="Apellido paterno"
-                />
-              </div>
-              <div className="col-lg-6 col-md-6  col-xs-12 pr-lg-2 pr-md-2 pb-sm-3 pb-xs-3">
-                <TextField
-                  name="segundoApellidoDoc"
-                  format="uppercase"
-                  formulario={formulario}
-                  maxlength={20}
-                  type="text"
-                  size="big"
-                  label="Apellido materno"
-                />
-              </div>
-              <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 ">
-                <p className="input color-gray">Su correo es</p>
-              </div>
-              <div className="col-lg-6 col-md-6 col-xs-12 pb-sm-3 pb-xs-3">
-                <TextField
-                  name="correoDoc"
-                  formulario={formulario}
-                  size="big"
-                  label="Correo@mail.com"
-                  type="email"
-                  format="email"
-                />
-              </div>
-              <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 ">
-                <p className="input color-gray">Su teléfono es</p>
-              </div>
-              <div className="col-lg-6 col-md-6 col-xs-12 pb-sm-3 pb-xs-3">
-                <TextField
-                  name="celularDoc"
-                  formulario={formulario}
-                  type="tel"
-                  size="big"
-                  label="55-9999-9999"
-                  format="phone"
-                  maxlength={12}
-                />
-              </div>
-            </div>
-            <div className="flex-column-center-config pt-sm-5 pt-xs-5 pt-md-0 pt-lg-0">
-              <button
-                type="button"
-                className="btn-big flex-align-self-center my-3"
-                disabled={!(formulario.isValid && formulario.dirty)}
-                onClick={() => setOpenConfirmation(true)}
-              >
-                Notificar al obligado
+          </div>
+          <div className="d-flex justify-content-center">
+            <Link href="/solicitud/[tab]/[step]" as="/solicitud/documentacion/3">
+              <button className="btn-medium" type="submit" aria-label="Avanzar">
+                <span>Continuemos</span>
               </button>
-            </div>
-          </form>
+            </Link>
+          </div>
+        </div>
+      </Modal>
+      <div className="contedor-fixed-tab">
+        <div className="contedor-solicitud ">
+          <div className="container p-0 mt-5">
+            <form className="mt-xs-5 mt-md-0 mt-lg-0" onSubmit={formulario.handleSubmit} noValidate>
+              <h2 className="color-blue-storm">¡Anotado!</h2>
+              <p className="color-dark-gray sub">
+                También requerimos que tu Obligado Solidario realice un proceso similar al tuyo, por lo que te pedimos
+                nos compartas sus datos para invitarle.
+              </p>
+
+              <div className="row no-gutters">
+                <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 ">
+                  <p className="input color-gray">Mi nombre es</p>
+                </div>
+                <div className="col-lg-4 col-md-4  col-xs-12 pr-lg-2 pr-md-2 pb-sm-3 pb-xs-3">
+                  <TextField
+                    name="primerNombreDoc"
+                    format="uppercase"
+                    maxlength={12}
+                    formulario={formulario}
+                    type="text"
+                    size="big"
+                    label="Nombre"
+                  />
+                </div>
+                <div className="col-lg-4 col-md-4  col-xs-12 pr-lg-2 pr-md-2 pb-sm-3 pb-xs-3">
+                  <TextField
+                    name="segundoNombreDoc"
+                    format="uppercase"
+                    maxlength={60}
+                    formulario={formulario}
+                    type="text"
+                    size="big"
+                    label="Nombre"
+                    optional
+                  />
+                </div>
+                <div className="col-lg-6 col-md-6  col-xs-12 pr-lg-2 pr-md-2 pb-sm-3 pb-xs-3">
+                  <TextField
+                    name="primerApellidoDoc"
+                    format="uppercase"
+                    maxlength={20}
+                    formulario={formulario}
+                    type="text"
+                    size="big"
+                    label="Apellido paterno"
+                  />
+                </div>
+                <div className="col-lg-6 col-md-6  col-xs-12 pr-lg-2 pr-md-2 pb-sm-3 pb-xs-3">
+                  <TextField
+                    name="segundoApellidoDoc"
+                    format="uppercase"
+                    formulario={formulario}
+                    maxlength={20}
+                    type="text"
+                    size="big"
+                    label="Apellido materno"
+                  />
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 ">
+                  <p className="input color-gray">Su correo es</p>
+                </div>
+                <div className="col-lg-6 col-md-6 col-xs-12 pb-sm-3 pb-xs-3">
+                  <TextField
+                    name="correoDoc"
+                    formulario={formulario}
+                    size="big"
+                    label="correo@mail.com"
+                    type="email"
+                    format="email"
+                  />
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 ">
+                  <p className="input color-gray">Su teléfono es</p>
+                </div>
+                <div className="col-lg-6 col-md-6 col-xs-12 pb-sm-3 pb-xs-3">
+                  <TextField
+                    name="celularDoc"
+                    formulario={formulario}
+                    type="tel"
+                    size="big"
+                    label="55-9999-9999"
+                    format="phone"
+                    maxlength={12}
+                  />
+                </div>
+              </div>
+              <div className="flex-column-center-config pt-sm-5 pt-xs-5 pt-md-0 pt-lg-0">
+                <button
+                  type="button"
+                  className="btn-big flex-align-self-center my-3"
+                  disabled={!(formulario.isValid && formulario.dirty)}
+                  onClick={() => setOpenConfirmation(true)}
+                >
+                  Notificar al obligado
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default StepTwo;
