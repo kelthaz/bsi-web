00ef7655c3c156237f64e7d7ba56922b3605d71d
Mateo Leal <mateo.leal@the-cocktail.com>
2021-01-05 15:59:05 -0500

    DEVUS: PYMC-628: Front| Documentación moral preguntas de auditoría (pantallas 5-7) (Eliminacion de import inutilizado)

--- components/features/solicitud/documentacion/StepFive.js
+++ components/features/solicitud/documentacion/StepFive.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
 import { useRouter } from 'next/router';
