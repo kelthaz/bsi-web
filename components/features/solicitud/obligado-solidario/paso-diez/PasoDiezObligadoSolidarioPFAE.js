664c88cd087449dc7972710ea2f3ec9059e6f064
David Prado <carlos.prado@the-cocktail.com>
2021-02-23 19:32:53 -0500

    rutas y state obligado solidario

---
+++ components/features/solicitud/obligado-solidario/paso-diez/PasoDiezObligadoSolidarioPFAE.js
@@ -0,0 +1,162 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFormik } from 'formik';
+import { useRouter } from 'next/router';
+import * as Yup from 'yup';
+import { nextStepDatosPersonales } from '../../../../../redux/actions/solicitud';
+import FileInput from '../../../../shared/file-input/FileInput';
+import Modal from '../../../../shared/modal/Modal';
+import Tooltip from '../../../../shared/tooltip/Tooltip';
+import { campoRequerido } from '../../../../../constants/errors';
+import useOnChangePage from '../../../../../hooks/useOnChangePage';
+import { PASO_ONCE_OBLIGADO_SOLIDARIO_ROUTE } from '../../../../../constants/routes/solicitud/obligado';
+
+const PasoDiezObligadoSolidarioPFAE = () => {
+  const { obligadoSolidario, currentStep } = useSelector((state) => state.solicitud);
+  const [openConfirmation, setOpenConfirmation] = useState(false);
+  const dispatch = useDispatch();
+  const { query } = useRouter();
+  const validate = currentStep.step === query.step;
+
+  const { initialValues, validationSchema } = {
+    initialValues: {
+      comprobanteDomicilioFiscal: obligadoSolidario.comprobanteDomicilioFiscal,
+      actaMatrimonio: obligadoSolidario.actaMatrimonio,
+      ineRepresentanteLegal: obligadoSolidario.ineRepresentanteLegal,
+      ineReversoRepresentanteLegal: obligadoSolidario.ineReversoRepresentanteLegal,
+    },
+    validationSchema: {
+      comprobanteDomicilioFiscal: Yup.string().url().required(campoRequerido),
+      ineRepresentanteLegal: Yup.string().url().required(campoRequerido),
+      ineReversoRepresentanteLegal: Yup.string().url().required(campoRequerido),
+    },
+  };
+
+  if (obligadoSolidario.bienesSeparados !== 'no') {
+    initialValues.actaMatrimonio = obligadoSolidario.actaMatrimonio;
+    initialValues.inePareja = obligadoSolidario.inePareja;
+    initialValues.ineReversoPareja = obligadoSolidario.ineReversoPareja;
+    validationSchema.actaMatrimonio = Yup.string().url().required(campoRequerido);
+    validationSchema.inePareja = Yup.string().url().required(campoRequerido);
+    validationSchema.ineReversoPareja = Yup.string().url().required(campoRequerido);
+  }
+
+  const formulario = useFormik({
+    initialValues,
+    onSubmit: (values) => {
+      dispatch(
+        nextStepDatosPersonales({
+          currentStep: validate ? { tab: 'preguntas', step: '11' } : { ...currentStep },
+          obligadoSolidario: { ...obligadoSolidario, ...values },
+        })
+      );
+    },
+  });
+
+  const [handleSubmit] = useOnChangePage(formulario, PASO_ONCE_OBLIGADO_SOLIDARIO_ROUTE, currentStep);
+
+  return (
+    <>
+      <Modal openModal={openConfirmation} setOpenModal={setOpenConfirmation}>
+        <div className="container px-xs-0 px-md-0 modal-container-video">
+          <div>
+            <h4 className="color-blue-storm">Recomendaciones para tus documentos</h4>
+            <p className="dark-gray body2">
+              Para garantizar que recibamos tus documentos en la mejor calidad posible, te recomendamos lo siguiente:
+            </p>
+            <p className="color-blue-sky sub offset-md-3">600 dpi | Formato PDF e imagen</p>
+            <p className="dark-gray body2">Si no cuentas con tus documentos escaneados te recomendamos dos opciones:</p>
+            <p className="dark-gray body2">1. Ir a un establecimiento a escanearlos.</p>
+            <p className="dark-gray body2">
+              2. Descarga la app “Microsoft Office Lens” en tu smartphone (iOS y Android) y escanea tus documentos.
+            </p>
+          </div>
+          <div className="flex-column-center-config mt-2">
+            <button onClick={() => setOpenConfirmation(false)} type="submit" className="btn-big">
+              Continuar
+            </button>
+          </div>
+        </div>
+      </Modal>
+      <div className="contedor-fixed-tab">
+        <div className="contedor-solicitud">
+          <div className="container pl-md-3 pl-xs-0">
+            <form className="mt-xs-5 mt-md-0 mt-lg-0" onSubmit={handleSubmit} noValidate>
+              <div className="row px-md-3 px-xs-0">
+                <p className="color-dark-gray sub">
+                  Con base en tu respuesta, necesitamos que nos proporciones los siguientes documentos:
+                  <Tooltip message="Puedes cargar documentos en formato PNG, JPG o PDF, con un peso máximo de 5 MB" />
+                </p>
+              </div>
+              <div className="row px-md-3 px-xs-0">
+                {obligadoSolidario.bienesSeparados !== 'no' ? (
+                  <div>
+                    <div className="col-md-12 pb-md-4">
+                      <FileInput
+                        formulario={formulario}
+                        name="comprobanteDomicilio"
+                        text="Comprobante de domicilio"
+                        grayText="(No mayor a tres meses)"
+                      />
+                    </div>
+                    <div className="col-md-12 pb-md-4">
+                      <FileInput text="Acta de matrimonio" />
+                    </div>
+                    <div className="col-md-12 pb-md-4">
+                      <FileInput text="Tu INE" subText="Por el frente" />
+                    </div>
+                    <div className="col-md-12 pb-md-4">
+                      <FileInput text="Tu INE" subText="Por el reverso" />
+                    </div>
+                    <div className="col-md-12 pb-md-4">
+                      <FileInput text="INE de tu pareja" subText="Por el frente" />
+                    </div>
+                    <div className="col-md-12 pb-md-4">
+                      <FileInput text="INE de tu pareja" subText="Por el reverso" />
+                    </div>
+                  </div>
+                ) : (
+                  <div>
+                    <div className="col-md-12 pb-md-4">
+                      <FileInput
+                        formulario={formulario}
+                        name="comprobanteDomicilio"
+                        text="Comprobante de domicilio"
+                        grayText="(No mayor a tres meses)"
+                      />
+                    </div>
+                    <div className="col-md-12 pb-md-4">
+                      <FileInput text="Tu INE" subText="Por el frente" />
+                    </div>
+                    <div className="col-md-12 pb-md-4">
+                      <FileInput text="Tu INE" subText="Por el reverso" />
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className="row ">
+                <p className="color-dark-gray sub">
+                  Haz{' '}
+                  <button type="button" className="btn-link" onClick={() => setOpenConfirmation(true)}>
+                    clic aquí
+                  </button>{' '}
+                  si no cuentas con tus documentos escaneados o quieres saber nuestras recomendaciones para tus
+                  documentos.
+                </p>
+              </div>
+              <div className="flex-column-center-config">
+                <button
+                  type="submit"
+                  //   disabled={!(formulario.dirty && formulario.isValid)}
+                  className="cicle-button-blue my-3"
+                  aria-label="Avanzar"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default PasoDiezObligadoSolidarioPFAE;
---
+++ components/features/solicitud/obligado-solidario/paso-uno/pfae/PasoUnoObligadoSolidarioPFAE.js
@@ -0,0 +1,94 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useRouter } from 'next/router';
+import {
+  campoRequerido,
+  longitudMaxima,
+  codigoPostalInvalido,
+  seleccionOpcion,
+} from '../../../../../../constants/errors';
+import Domicilio from '../../../shared/domicilio/Domicilio';
+import useOnChangePage from '../../../../../../hooks/useOnChangePage';
+import { PASO_DOS_OBLIGADO_SOLIDARIO_ROUTE } from '../../../../../../constants/routes/solicitud/obligado';
+import { nextStepObligadoSolidario } from '../../../../../../redux/actions/obligado';
+
+const PasoUnoObligadoSolidarioPFAE = () => {
+  const { currentStep, obligadoSolidario } = useSelector((state) => state.solicitud);
+  const dispatch = useDispatch();
+  const { query } = useRouter();
+  const validate = currentStep.step === query.step;
+
+  const formulario = useFormik({
+    initialValues: {
+      domicilioFiscal: { ...obligadoSolidario.domicilioFiscal },
+    },
+    validationSchema: Yup.object({
+      domicilioFiscal: Yup.object().shape({
+        calle: Yup.string().max(60, longitudMaxima).required(campoRequerido),
+        numExterior: Yup.string().max(6, longitudMaxima).required(campoRequerido),
+        numInterior: Yup.string().max(6, longitudMaxima),
+        codigoPostal: Yup.string().min(5, codigoPostalInvalido).max(5, longitudMaxima).required(campoRequerido),
+        colonia: Yup.object()
+          .shape({
+            value: Yup.string(),
+            label: Yup.string(),
+          })
+          .nullable()
+          .required(seleccionOpcion),
+        municipioAlcaldia: Yup.string(),
+        ciudad: Yup.string(),
+        estado: Yup.string(),
+      }),
+    }),
+    onSubmit: (values) => {
+      dispatch(
+        nextStepObligadoSolidario({
+          currentStep: validate ? { tab: 'preguntas', step: '2' } : { ...currentStep },
+          obligadoSolidario: { ...obligadoSolidario, ...values },
+        })
+      );
+    },
+  });
+
+  const [handleSubmit] = useOnChangePage(formulario, PASO_DOS_OBLIGADO_SOLIDARIO_ROUTE, currentStep);
+
+  return (
+    <>
+      <div className="contedor-fixed-tab">
+        <div className="contedor-solicitud ">
+          <div className="container p-0">
+            <form onSubmit={handleSubmit} noValidate>
+              <p className="color-dark-gray sub">
+                Alejandra ya nos ha platicado un poco sobre ti, por lo que te pediremos información adicional para
+                completar tu expediente y habremos terminado.
+              </p>
+              <p className="color-dark-gray sub">Para comenzar, por favor compártenos tu domicilio.</p>
+              <Domicilio
+                formulario={formulario}
+                nameFieldCalle="domicilioFiscal.calle"
+                nameFieldNumExterior="domicilioFiscal.numExterior"
+                nameFieldNumInterior="domicilioFiscal.numInterior"
+                nameFieldCodigoPostal="domicilioFiscal.codigoPostal"
+                nameFieldColonia="domicilioFiscal.colonia"
+                nameFieldMunicipioAlcaldia="domicilioFiscal.municipioAlcaldia"
+                nameFieldCiudad="domicilioFiscal.ciudad"
+                nameFieldEstado="domicilioFiscal.estado"
+              />
+
+              <div className="flex-column-center-config">
+                <button
+                  type="submit"
+                  className="cicle-button-blue my-3"
+                  aria-label="Avanzar"
+                  disabled={validate && !(formulario.isValid && formulario.dirty)}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default PasoUnoObligadoSolidarioPFAE;
