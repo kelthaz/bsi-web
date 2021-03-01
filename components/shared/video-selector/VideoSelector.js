684cdc4845e20a5ffd6a4c37b01aaadafa1da83e
David Prado <carlos.prado@the-cocktail.com>
2021-03-01 16:29:24 -0500

    Bugfix: 798

--- components/shared/video-selector/VideoSelector.js
+++ components/shared/video-selector/VideoSelector.js
@@ -1,66 +1,10 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
+import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 import styles from './video-selector.module.scss';
 
-const sliderOptions = [
-  {
-    id: 1,
-    active: true,
-    src: 'https://www.youtube.com/embed/mJZYMWDL_0o',
-    title: 'Ismael García López / Nuevo Grupo Visual SA de CV',
-    subtitle: 'Atención cálida para asegurar tu futuro | BanCoppel Pymes',
-    parragraph:
-      'En BanCoppel procuramos atenderte de una manera cálida y humana para que tu empresa tenga la estabilidad económica que necesita a través de un proceso ágil y personalizado asegurando así, el flujo de capital necesario para avanzar hacia el futuro. Ingresa a www.bancoppel.com/empresas y conoce todo lo que tenemos para tí.',
-  },
-  {
-    id: 2,
-    active: false,
-    src: 'https://www.youtube.com/embed/VayHlVwGmGs',
-    title: 'Óscar Vélez González / PIZZETA SA de CV ',
-    subtitle: 'Somos el aliado que tu empresa necesita | BanCoppel Pymes',
-    parragraph:
-      'Nos aseguramos de que tengas una verdadera buena experiencia con nosotros para que puedas crecer como tu empresa lo merece. Ingresa a www.bancoppel.com/empresas y conoce todo lo que tenemos para tí.',
-  },
-  {
-    id: 3,
-    active: false,
-    src: 'https://www.youtube.com/embed/7DOtcx4pnuo',
-    title: 'Sandra Quintana / Compupartes y Accesorios SA de CV ',
-    subtitle: 'No dejes ir ningún proyecto por falta de liquidez | BanCoppel Pymes',
-    parragraph:
-      'Queremos ser los artífices de tu futuro, por lo que te ofrecemos un crédito ajustado a tus necesidades. ¿Necesitas escalar tus operaciones? Ponte en contacto con nosotros ingresando a www.bancoppel.com/empresas y te ayudaremos a hacer tus sueños realidad.',
-  },
-  {
-    id: 4,
-    active: false,
-    src: 'https://www.youtube.com/embed/ELgjL7oe8AY',
-    title: 'Alberto Kichik Sidauy / CAMISAS IMPALA SA de CV ',
-    subtitle: 'Estamos ahí para tu crecimiento | BanCoppel Pymes',
-    parragraph:
-      'En BanCoppel estamos ahí para tu crecimiento, y procuraremos que, con empatía y humanidad, ser los propulsores de tu éxito. Ponte en contacto con nosotros en www.bancoppel.com/empresas.',
-  },
-  {
-    id: 5,
-    active: false,
-    src: 'https://www.youtube.com/embed/ppIJmaa9ue8',
-    title: 'Antonio Barón / TECNOSOCKS SA de CV ',
-    subtitle: 'Sabemos escucharte | BanCoppel Pymes',
-    parragraph:
-      'En BanCoppel, estamos dispuestos a escucharte y brindarte la atención que necesitas. Si quieres que tu pyme alcance el éxito, no dudes en ponerte en contacto con nosotros. www.bancoppel.com/empresas.',
-  },
-  {
-    id: 6,
-    active: false,
-    src: 'https://www.youtube.com/embed/PMP6zcY6Fcc',
-    title: 'Rubén Fresan / BEER- LINMEX SA de CV',
-    subtitle: 'Te ofrecemos un trato cálido, cercano y entendemos tus necesidades',
-    parragraph:
-      'En BanCoppel, no solo las condiciones crediticias son importantes, también te ofrecemos un trato cálido y cercano para no te sientas solo en tu camino al éxito. No dudes en ponerte con nosotros: www.bancoppel.com/empresas.',
-  },
-];
-
-const VideoSelector = ({ color }) => {
+const VideoSelector = ({ color, sliderOptions }) => {
   const [currentSlide, setCurrentSlide] = useState(1);
 
   const updateSlide = (direction) => {
@@ -107,6 +51,7 @@ const VideoSelector = ({ color }) => {
       <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 offset-lg-6 offset-md-6  ">
         <div className="ml-xs-0 ml-md-4 ml-lg-0">
           <h4 className={color === 'blue-storm' ? 'text-white' : 'text-primary'}>{option.title}</h4>
+          <p className={`body2 ${styles['text-company-gray']}`}>{option.company}</p>
           {option.subtitle && <p className={styles['text-gray']}>{option.subtitle}</p>}
           <p className={`body2 ${color === 'blue-storm' ? 'text-white' : 'text-dark'}`}>{option.parragraph}</p>
         </div>
@@ -141,6 +86,14 @@ const VideoSelector = ({ color }) => {
       </div>
     </div>
   );
+};
+
+VideoSelector.propType = {
+  sliderOptions: PropTypes.array,
+};
+
+VideoSelector.defaultProps = {
+  sliderOptions: [],
 };
 
 export default VideoSelector;
