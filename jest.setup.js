244613d3597adabc7e126f16bec1ce598810d8f7
Jesus David Gomez Osorno <jesus.gomez@the-cocktail.com>
2021-03-03 09:20:23 -0500

    DEVUS: ajustes del host de los servicios

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,5 @@
-import Adapter from 'enzyme-adapter-react-16';
+// import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { configure } from 'enzyme';
 
 configure({ adapter: new Adapter() });
