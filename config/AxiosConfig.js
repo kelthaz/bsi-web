e8b8f2cb6408ffca79cc4db337fd4d7f46059630
CEIBA\yuliana.canas <yuliana.canas@ceiba.com.co>
2021-03-04 10:28:20 -0500

    Se retrocede en el cambio anterior

--- config/AxiosConfig.js
+++ config/AxiosConfig.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log({value: process.env.NEXT_PUBLIC_API_BANCOPPEL_PYME_HOST})
-
 const axiosIntance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BANCOPPEL_PYME_HOST,
   // baseURL: process.env.NEXT_PUBLIC_PYME_HOSTNAME,
