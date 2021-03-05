96699236e71a20f9dbec3ab48b6a7d25e7bb5734
CEIBA\yuliana.canas <yuliana.canas@ceiba.com.co>
2021-03-05 10:22:21 -0500

    Cambio para usar variable de entorno

--- config/AxiosConfig.js
+++ config/AxiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log({value: process.env.NEXT_PUBLIC_API_BANCOPPEL_PYME_HOST})
 
 const axiosIntance = axios.create({
-  baseURL: 'https://bsi-cred-pyme-service-bsi-cr-pyme-web-stg.obe8.bancoppel-dev.openshiftapps.com/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BANCOPPEL_PYME_HOST,
   // baseURL: process.env.NEXT_PUBLIC_PYME_HOSTNAME,
   timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
