f5be302cefe786d85ad3b65c9110ddd92411b622
Jesus David Gomez Osorno <jesus.gomez@the-cocktail.com>
2021-04-12 14:12:56 -0500

    DEVUS: PYMC-860 (Se agrego la función de expand)

--- components/shared/visor/VisorPDF.js
+++ components/shared/visor/VisorPDF.js
@@ -5,20 +5,15 @@ import styles from './visor-pdf.module.scss';
 
 const VisorPDF = ({ data, extension = 'pdf', container }) => {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-  const FIT_LABELS = {
-    page: 'Fit to Page',
-    width: 'Fit to Width',
-  };
+
   const pageRef = useRef();
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [scale, setScale] = useState(1);
-  const [fitTo, setFitTo] = useState();
-  const [grades, setGrades] = useState(0);
+  const [degree, setDegree] = useState(0);
+  const [expand, setExpand] = useState(false);
 
   const [url, setUrl] = useState('');
-
-  // console.log(container);
 
   useEffect(async () => {
     const tablaPdf = await SimuladorRepositorio.postSimuladorTablaPdf({
@@ -34,31 +29,48 @@ const VisorPDF = ({ data, extension = 'pdf', container }) => {
   const onDocumentLoadSuccess = ({ numPages: nPages }) => setNumPages(nPages);
 
   const onPageLoadSuccess = (page) => {
-    // pageRef.current = page;
-    console.log(page);
+    pageRef.current = page;
+    console.log(pageRef.current);
   };
 
   const changePage = (offset) => setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  const changeDegree = (degreesAdd) =>
+    setDegree((prevDegreesAdd) =>
+      degreesAdd + prevDegreesAdd >= 360 ? degreesAdd + prevDegreesAdd - 360 : degreesAdd + prevDegreesAdd
+    );
 
   const previousPage = () => changePage(-1);
 
   const nextPage = () => changePage(1);
 
-  const turnRight = () => setGrades(90 + grades);
+  const turnRight = () => changeDegree(90);
 
-  const turnLeft = () => setGrades(270 + grades);
-
-  const nextFit = (fitTo) => (fitTo === 'page' ? 'width' : 'page');
+  const turnLeft = () => changeDegree(270);
 
   const onZoomIn = () => setScale(scale * 1.1);
   const onZoomOut = () => setScale(scale * 0.9);
 
+  const calculateScale = () => {
+    const { clientWidth, clientHeight } = container.current;
+    const { originalWidth, originalHeight } = pageRef.current;
+
+    const rotated = degree % 180 !== 0;
+    const pageWidth = rotated ? originalHeight : originalWidth;
+    const pageHeight = rotated ? originalWidth : originalHeight;
+    const scaleX = clientWidth / pageWidth;
+    const scaleY = clientHeight / pageHeight;
+
+    setExpand(!expand);
+    setScale(expand ? scale * scaleX : scale * Math.min(scaleX, scaleY));
+  };
+
+  console.log(degree % 180 !== 0 ? 'rotate' : 'no rotate');
   console.log(pageRef.current);
 
   return (
     <div className={styles['contenedor-pdf']}>
       <Document file="/Hunk-160R-FI-Hero-MotoCorp.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} scale={scale} rotate={grades} onLoadSuccess={onPageLoadSuccess} ref={pageRef} />
+        <Page pageNumber={pageNumber} scale={scale} rotate={degree} onLoadSuccess={onPageLoadSuccess} ref={pageRef} />
       </Document>
 
       <div>
@@ -78,13 +90,13 @@ const VisorPDF = ({ data, extension = 'pdf', container }) => {
           <button type="button" disabled={pageNumber >= numPages} onClick={onZoomOut}>
             -
           </button>
-          <button type="button" disabled={pageNumber >= numPages} onClick={() => onZoomToFit(nextFit(fitTo))}>
+          <button type="button" disabled={pageNumber >= numPages} onClick={calculateScale}>
             expand
           </button>
-          <button type="button" disabled={pageNumber >= numPages} onClick={() => turnLeft()}>
+          <button type="button" disabled={pageNumber >= numPages} onClick={turnLeft}>
             izquierda
           </button>
-          <button type="button" disabled={pageNumber >= numPages} onClick={() => turnRight()}>
+          <button type="button" disabled={pageNumber >= numPages} onClick={turnRight}>
             derecha
           </button>
         </div>
