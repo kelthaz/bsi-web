f74363533fded0838271cdc8370d6c5fde5a47bd
David Prado <carlos.prado@the-cocktail.com>
2021-04-27 09:51:11 -0500

    Formulario analista juridico

---
+++ components/svgs/SvgInfoBackOffice.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+function SvgInfoBackOffice(props) {
+  return (
+    <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 4v16a2 2 0 002 2h12a2 2 0 002-2v-8a2 2 0 00-2-2h-5a1 1 0 01-1-1V4a2 2 0 00-2-2H6a2 2 0 00-2 2zm3.987 10.01c0 .552.448 1 1 1.002L15 15.03a.996.996 0 001-.997c0-.552-.448-1.002-1-1.003l-6.013-.018a.996.996 0 00-1 .997zm1 5c-.552-.002-1-.45-1-1.003a.996.996 0 011-.997l4.419.013c.552.002 1 .45 1 1.003a.996.996 0 01-1 .997l-4.42-.013zM18.069 9H14a1 1 0 01-1-1V3.931a.8.8 0 011.366-.565l4.268 4.268A.8.8 0 0118.07 9z"
+        fill="#225AA7"
+      />
+    </svg>
+  );
+}
+
+export default SvgInfoBackOffice;
